fa7ac28126ecfd6c888cc48c55b1e38d077c6d22
Max Diamond <diamax98@gmail.com>
2022-09-05 16:06:20 -0400

    Added Card class and built out SetPage class

--- selenium_test.py
+++ selenium_test.py
@@ -32,6 +32,45 @@ class SetsPage:
         return [Expansion(name=name, url=url, card_count=None, cards=None) for name,url in zip(names, urls)]
 
 
+class Card:
+    """Represents an individual pokemon card. Comprised of the card name, card number, and total number of cards in the set (excluding "special" types of rare cards)
+    """
+    def __init__(self, name, card_number, card_count):
+        self.name = name
+        self.card_number = card_number
+        self.card_count = card_count
+
+    def __str__(self):
+        return f'{self.name} {self.card_number}/{self.card_count}'
+
+
+class SetPage:
+    def __init__(self, page_source):
+        self.etree = etree.HTML(page_source)
+        self.card_row_locator = ".//tr[contains(text(), '/')]/.."
+        self.set_number_locator = ".//tr[contains(text(), '/')]"
+
+    @property
+    def set_count(self):
+        set_number = self.etree.xpath(self.set_number_locator).text
+        set_count = set_number.split('/')[-1].strip()
+        return set_count
+
+    def get_card_num_and_set_count(self, row):
+        set_number = row.xpath(self.set_number_locator)
+        (card_num,set_count) = [el.strip() for el in set_number.split('/')]
+        return card_num, set_count
+
+    def get_card_name(self, row):
+        name = row.find(".//td[@width='20%']/a").text
+        return name
+
+    def get_cards(self):
+        rows = self.etree.findall(self.card_row_locator)
+        cards = [Card(self.get_card_name(row), self.get_card_num_and_set_count(row)) for row in rows]
+        return cards
+
+
 def main():
     driver = webdriver.Chrome()
     driver.get(english_expansions_url)
@@ -39,6 +78,11 @@ def main():
     # Test
     english_sets_page = SetsPage(driver.page_source)
     astral_radiance = english_sets_page.get_expansion_names_and_urls()[3]
+    driver.get(astral_radiance.url)
+    astral_radiance_set = SetPage(driver.page_source)
+    astral_radiance.card_count = astral_radiance_set.set_count
+    astral_radiance.cards = astral_radiance_set.get_cards()
+    print(astral_radiance.cards[-12])
     print(astral_radiance)
     print('done')
 
