a0ec73870c4fe9c22adff3376de882aebef0b711
Max Diamond <diamax98@gmail.com>
2022-09-11 20:20:26 -0400

    Updated xpath parsing, beginning to update get_card_name()

--- components/web.py
+++ components/web.py
@@ -1,21 +1,18 @@
 import requests
 from lxml import etree
-from io import StringIO
-from bs4 import BeautifulSoup
 
-from components import cards
+from components.cards import Card, Expansion
+import constants
+
+
+serebii_base_url = 'https://www.serebii.net'
 
 
 class Soup:
     def __init__(self, url):
         self.url = url
         self.r = requests.get(self.url, verify=False)
-        self.soup = BeautifulSoup(self.r.text, 'html.parser')
-        self.tree = etree.parse(StringIO(self.soup.get_text()), etree.HTMLParser())
         self.html = etree.HTML(self.r.text)
-        # self.html = etree.HTML(self.soup.get_text())
-        self.etree_ = etree.tostring(self.tree.getroot(), pretty_print=True)
-        self.etree2 = etree.tostring(self.html, pretty_print=True)
 
 
 class AllSetsPage(Soup):
@@ -23,24 +20,25 @@ class AllSetsPage(Soup):
         super().__init__(url)
 
     def get_expansion_names_and_urls(self):
-        locator = ".//tr/td/a"
-        matches = self.soup.findall(locator)
+        locator = "//tr/td/a"
+        matches = self.html.xpath(locator)
         # Only want match to expansion name not expansion logo
         expansions = [a for a in matches if matches.index(a)%2==0]
         names = [a.text for a in expansions]
-        urls = [self.url + a.attrib['href'] for a in expansions]
-        return [cards.Expansion(name=name, url=url, card_count=None, cards=None) for name,url in zip(names, urls)]
+        urls = [constants.serebii_base_url + a.attrib['href'] for a in expansions]
+        return [Expansion(name=name, url=url, card_count=None, cards=None) for name,url in zip(names, urls)]
 
 
 class SetPage(Soup):
     def __init__(self, url):
         super().__init__(url)
-        self.card_row_locator = ".//tr//text() == '/')]/.."
-        self.set_number_locator = ".//tr[contains(text(), '/')]"
+        self.card_row_locator = "//tr/td[contains(text(), '/')]/.."
+        self.set_number_locator = "//tr/td[contains(text(), '/')]/text()"  # find "# / # "
 
     @property
     def set_count(self):
-        set_number = self.soup.xpath(self.set_number_locator).text
+        # Only grab the first match
+        set_number = self.html.xpath(self.set_number_locator)[0]
         set_count = set_number.split('/')[-1].strip()
         return set_count
 
@@ -50,10 +48,12 @@ class SetPage(Soup):
         return card_num, set_count
 
     def get_card_name(self, row):
-        name = row.find(".//td[@width='20%']/a").text
+        main_name = row.xpath(".//td[@width='20%']/a/font/text()")
         return name
 
+    def clean_card_name(self, font_tag_text, a_tag_text):
+
     def get_cards(self):
-        rows = self.soup.findall(self.card_row_locator)
-        cards = [cards.Card(self.get_card_name(row), self.get_card_num_and_set_count(row)) for row in rows]
+        rows = self.html.xpath(self.card_row_locator)
+        cards = [Card(self.get_card_name(row), self.get_card_num_and_set_count(row)) for row in rows]
         return cards
