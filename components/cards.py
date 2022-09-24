936db740b2de21321a5c67fb41387e3db11a3a1c
Max Diamond <diamax98@gmail.com>
2022-09-24 12:12:27 -0400

    Renamed card_ct -> set_ct

--- components/cards.py
+++ components/cards.py
@@ -1,21 +1,21 @@
 class Expansion:
-    def __init__(self, name, url, card_count, cards):
+    def __init__(self, name, url, set_count, cards):
         self.name = name
         self.url = url
-        self.card_count = card_count
+        self.set_count = set_count
         self.cards = cards
 
     def __str__(self):
-        return f'The {self.name} expansion has {self.card_count} cards'
+        return f'The {self.name} expansion has {self.set_count} cards'
 
 
 class Card:
     """Represents an individual pokemon card. Comprised of the card name, card number, and total number of cards in the set (excluding "special" types of rare cards)
     """
-    def __init__(self, name, card_number, card_count):
+    def __init__(self, name, card_number, set_count):
         self.name = name
         self.card_number = card_number
-        self.card_count = card_count
+        self.set_count = set_count
 
     def __str__(self):
-        return f'{self.name} {self.card_number}/{self.card_count}'
+        return f'{self.name} {self.card_number}/{self.set_count}'
